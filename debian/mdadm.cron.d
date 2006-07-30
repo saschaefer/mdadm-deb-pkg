2afbf9cf0fe8329ed033f24be3d80ac074af853e
madduck <madduck@3cfab66f-1918-0410-86b3-c06b76f9a464>
2006-07-30 10:09:09 +0000

    * checkarray: skip sync for RAID0 devices (closes: #379352, #380424). * Fixing cron registration for checkarray to really only run once a month.   crontab(5) is really stupid and makes me think that they simply documented   a bug instead of fixing it (closes: #380425).

--- debian/mdadm.cron.d
+++ debian/mdadm.cron.d
@@ -5,5 +5,7 @@
 # distributed under the terms of the Artistic Licence.
 #
 
-# by default, run at 01:06 on the first Sunday of each month.
-6 1 1-7 * 7 root [ -x /usr/share/mdadm/checkarray ] && /usr/share/mdadm/checkarray --cron --all --quiet
+# by default, run at 01:06 on the 5th of every month. I would like to be able
+# to limit this down to weekends, but crontab(5) is not able to do so
+# (see #380425)
+6 1 5 * * root [ -x /usr/share/mdadm/checkarray ] && /usr/share/mdadm/checkarray --cron --all --quiet
