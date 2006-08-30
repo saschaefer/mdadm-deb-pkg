539aa9d2fbbef1bdb3a62dcd7bf3feab0313e958
madduck <madduck@3cfab66f-1918-0410-86b3-c06b76f9a464>
2006-08-30 09:26:11 +0000

    typo

--- debian/mdadm.cron.d
+++ debian/mdadm.cron.d
@@ -7,8 +7,8 @@
 # $Id$
 #
 
-# By default, run at 01:06 on the every Sunday, but do nothing unless the day
-# of the month is less than or equal to 7. Thus, only run on the first Sunday
-# of each month. crontab(5) sucks, unfortunately, so therefore this hack
-# (see #380425).
+# By default, run at 01:06 on every Sunday, but do nothing unless the day of
+# the month is less than or equal to 7. Thus, only run on the first Sunday of
+# each month. crontab(5) sucks, unfortunately, in this regard; therefore this
+# hack (see #380425).
 6 1 * * 0 root [ -x /usr/share/mdadm/checkarray ] && [ $(date +\%d) -le 7 ] && /usr/share/mdadm/checkarray --cron --all --quiet
